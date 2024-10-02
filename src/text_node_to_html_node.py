de205509322dd4af0a8546158179e7e9ff1c5ab7
mustafa-ozturk <mozturkpro@gmail.com>
2024-10-01 20:20:41 -0400

    add text_node_to_html_node

---
+++ src/text_node_to_html_node.py
@@ -0,0 +1,19 @@
+from leafnode import LeafNode
+
+def text_node_to_html_node(text_node) -> LeafNode:
+    match (text_node.text_type):
+        case ("text"):
+            return LeafNode(text_node.text)
+        case ("bold"):
+            return LeafNode(text_node.text, "b")
+        case ("italic"):
+            return LeafNode(text_node.text, "i")
+        case ("code"):
+            return LeafNode(text_node.text, "code")
+        case ("link"):
+            return LeafNode(text_node.text, "a", {"href": ""})
+        case ("image"):
+            return LeafNode("", "img", {"src": text_node.url, "alt": text_node.text})
+        case _:
+           print(text_node.text_type)
+           raise Exception("Invalid text type")
