7e4322a709cb3c4552ba21719f3d41f5d2285c87
mustafa-ozturk <mozturkpro@gmail.com>
2024-10-01 20:25:39 -0400

    move text_node_to_html_node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
-from textnode import TextNode
-
+from textnode import TextNode, text_node_to_html_node
+from leafnode import LeafNode
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -36,3 +36,40 @@ class TestTextNode(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+class TestTextNodeToHtmlNode(unittest.TestCase):
+    def test_text_type(self):
+        node = TextNode("test", "text")
+        node1 = LeafNode("test")
+        converted_node = text_node_to_html_node(node)
+        self.assertEqual(converted_node.to_html(), node1.to_html())
+    
+    def test_text_bold(self):
+        node = TextNode("test", "bold")
+        node1 = LeafNode("test", "b")
+        converted_node = text_node_to_html_node(node)
+        self.assertEqual(converted_node.to_html(), node1.to_html())
+
+    def test_text_italic(self):
+        node = TextNode("test", "italic")
+        node1 = LeafNode("test", "i")
+        converted_node = text_node_to_html_node(node)
+        self.assertEqual(converted_node.to_html(), node1.to_html())
+
+    def test_text_code(self):
+        node = TextNode("test", "code")
+        node1 = LeafNode("test", "code")
+        converted_node = text_node_to_html_node(node)
+        self.assertEqual(converted_node.to_html(), node1.to_html())
+
+    def test_text_link(self):
+        node = TextNode("test", "link", "urltest")
+        node1 = LeafNode("test", "a", {"href": ""})
+        converted_node = text_node_to_html_node(node)
+        self.assertEqual(converted_node.to_html(), node1.to_html())
+
+    def test_text_image(self):
+        node = TextNode("test", "image", "urltest")
+        node1 = LeafNode("", "img", {"src": "urltest", "alt": "test"})
+        converted_node = text_node_to_html_node(node)
+        self.assertEqual(converted_node.to_html(), node1.to_html())
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,4 @@
-
+from leafnode import LeafNode
 
 class TextNode():
     def __init__(self, text, text_type, url = None):
@@ -13,3 +13,20 @@ class TextNode():
     def __repr__(self):
         return f"TextNode({self.text},{self.text_type}, {self.url})"
 
+def text_node_to_html_node(text_node) -> LeafNode:
+    match (text_node.text_type):
+        case ("text"):
+            return LeafNode(text_node.text)
+        case ("bold"):
+            return LeafNode(text_node.text, "b")
+        case ("italic"):
+            return LeafNode(text_node.text, "i")
+        case ("code"):
+            return LeafNode(text_node.text, "code")
+        case ("link"):
+            return LeafNode(text_node.text, "a", {"href": ""})
+        case ("image"):
+            return LeafNode("", "img", {"src": text_node.url, "alt": text_node.text})
+        case _:
+           print(text_node.text_type)
+           raise Exception("Invalid text type")
