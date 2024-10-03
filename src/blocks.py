2e088e20e865672516c204f6b2d7b4739cf54b3d
mustafa-ozturk <mozturkpro@gmail.com>
2024-10-03 10:57:47 -0400

    add block to block type

--- src/blocks.py
+++ src/blocks.py
@@ -7,3 +7,54 @@ def markdown_to_blocks(markdown):
         if new_line != '':
             blocks.append(new_line)
     return blocks
+
+def block_to_block_type(block):
+    lines = block.split('\n\n')
+    if lines[0][0] == '#':
+        count = 1
+        for c in lines[0][1:]:
+            if c == ' ':
+                return "heading"
+            elif c == '#':
+                count += 1
+    elif lines[0][0] == '`':
+        if len(lines[0].split("```")) == 3:
+            return "code"
+    elif lines[0][0] == '>':
+        count = 1
+        for line in lines[1:]:
+            if line[0] == '>':
+                count += 1
+            else:
+                count = -1
+        if count  > 0:
+            return "quote"
+    elif lines[0][0] == '*' and lines[0][1] == ' ':
+        count = 1
+        for line in lines[1:]:
+            if line[0] == '*' and line[1] == ' ':
+                count += 1
+            else:
+                count = -1
+        if count > 0:
+            return "unordered_list"
+    elif lines[0][0] == '-' and lines[0][1] == ' ':
+        count = 1
+        for line in lines[1:]:
+            if line[0] == '-' and line[1] == ' ':
+                count += 1
+            else:
+                count = -1
+        if count  > 0:
+            return "unordered_list"
+    elif lines[0][0] == '1' and lines[0][1] == '.' and lines[0][2] == ' ':
+        count = 1
+        for line in lines[1:]:
+            if line[0] == str(count + 1) and line[1] == '.' and line[2] == ' ':
+                count += 1
+            else:
+                count = -1
+        if count  > 0:
+            return "ordered_list"
+    else:
+        return "paragraph"
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, block_to_block_type
 
 class TestMarkdownToBlovks(unittest.TestCase):
     def test_common_case(self):
@@ -49,3 +49,76 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
         markdown = ""
         blocks = markdown_to_blocks(markdown)
         self.assertEqual(len(blocks), 0)
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_heading(self):
+        type = block_to_block_type("###### heading")
+        expected_type = "heading"
+        self.assertEqual(type, expected_type)
+    
+    def test_code(self):
+        markdown = """```
+some code
+```
+"""
+        blocks = markdown_to_blocks(markdown)
+        type = block_to_block_type(blocks[0])
+        expected_type = "code"
+        self.assertEqual(type, expected_type)
+
+    def test_quote(self):
+        markdown = """
+> this
+> is
+> a 
+> quote
+"""
+        blocks = markdown_to_blocks(markdown)
+        type = block_to_block_type(blocks[0])
+        expected_type = "quote"
+        self.assertEqual(type, expected_type)
+
+    def test_unordered_list_star(self):
+        markdown = """
+* 1
+* 2
+* 3 
+"""
+        blocks = markdown_to_blocks(markdown)
+        type = block_to_block_type(blocks[0])
+        expected_type = "unordered_list"
+        self.assertEqual(type, expected_type)
+    
+    def test_unordered_list_dash(self):
+        markdown = """
+- 1
+- 2
+- 3 
+"""
+        blocks = markdown_to_blocks(markdown)
+        type = block_to_block_type(blocks[0])
+        expected_type = "unordered_list"
+        self.assertEqual(type, expected_type)
+
+    def test_ordered_list(self):
+        markdown = """
+1. one
+2. two
+3. three
+"""
+        blocks = markdown_to_blocks(markdown)
+        type = block_to_block_type(blocks[0])
+        expected_type = "ordered_list"
+        self.assertEqual(type, expected_type)
+    
+    def test_paragraph(self):
+        markdown = """
+hello world
+"""
+        blocks = markdown_to_blocks(markdown)
+        type = block_to_block_type(blocks[0])
+        expected_type = "paragraph"
+        self.assertEqual(type, expected_type)
+
+                                
