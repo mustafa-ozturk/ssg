298bceb75aef8296e785a4608af0dd407e4bf53a
mustafa-ozturk <mozturkpro@gmail.com>
2024-10-04 10:10:10 -0400

    add textnode tests

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from textnode import TextNode
+from textnode import TextNode, text_type_bold
 
 def main():
-    textnode = TextNode("boot.dev", "bold", "https://www.boot.dev")
+    textnode = TextNode("boot.dev", text_type_bold, "https://www.boot.dev")
     print("textnode:", textnode)
 
 
